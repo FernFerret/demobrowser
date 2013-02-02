2f01b6c84aefec286be6a177a3dc0f2c764bb7f3
Eric Stokes <fernferret@gmail.com>
2013-02-02 00:02:41 -0700

    More basic stuff

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from demobrowser import app, db
+#db.drop_all()
+db.create_all()
+app.debug = app.config.get('DEBUG', False)
+
+# This is required.
+app.secret_key = app.config.get('SECRET_KEY', 'I\'m a secret!11!!1')
+
+# TODO: Unhardcode this.
+app.run(host=app.config.get('ADDRESS', '127.0.0.1'))
